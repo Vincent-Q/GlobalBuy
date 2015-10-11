3f3b6e73f7e9e6ecdbd628acb04fb33b006750b5
System Administrator <root@VincentdeMacBook-Air.local>
2015-10-11 16:46:30 +0200

    make some changes

--- js/app.js
+++ js/app.js
@@ -29,13 +29,21 @@
 				}
 			}
 
-			var attrItemArr = attrs['useClass'].split(':');
-			var className = attrItemArr[0].trim();
-			var expression = attrItemArr[1].trim();
+			function watch(className, expression){
+				scope.$watch(expression, function(){
+					updateClass(className, expression);
+				}, true);
+			}
 
-			scope.$watch(expression, function(){
-				updateClass(className, expression);
-			}, true);
+			var attArr = attrs['useClass'].split(';');
+
+			for(var i=0; i<attArr.length; i++){
+				var attrItemArr = attArr[i].split(':');
+				var className = attrItemArr[0].trim();
+				var expression = attrItemArr[1].trim();
+
+				watch(className, expression);
+			}
 		}
 
 		return {
--- js/controllers/PackageListController.js
+++ js/controllers/PackageListController.js
@@ -4,6 +4,33 @@
 	mdl.controller('PackageListController', function(){
 		this.activeTab = "My Packages";
 
+		this.packageList = [{
+			'status': 'preparing',
+			'deliveryDate': '2015.10.07',
+			'goods': ' lush , Aptmil Milk Powder ',
+			'price': '$33.5'
+		}, {
+			'status': 'delivery',
+			'deliveryDate': '2015.09.30',
+			'goods': ' lush , Hipp Milk Powder ',
+			'price': '$37.5'
+		}, {
+			'status': 'finished',
+			'deliveryDate': '2015.09.25',
+			'goods': ' lush , Aptmil Milk Powder ',
+			'price': '$89.5'
+		}, {
+			'status': 'finished',
+			'deliveryDate': '2015.09.20',
+			'goods': ' Sausage , Hipp Milk Powder ',
+			'price': '$67.5'
+		}, {
+			'status': 'finished',
+			'deliveryDate': '2015.09.15',
+			'goods': ' lush , Aptmil Milk Powder ',
+			'price': '$33.5'
+		}];
+
 		this.click = function(activeTab){
 			this.activeTab = activeTab;
 		};
